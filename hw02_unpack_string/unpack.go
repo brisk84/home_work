c57c0a66e1668435a1638ce463ccc7ffd629a1db
Gumyarov Marat <brisk@inbox.ru>
2021-09-26 15:46:19 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,62 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+var (
+	prevRune rune
+	slash    bool
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func unpackRune(curRune rune) (string, error) {
+	var outString strings.Builder
+	if !unicode.IsDigit(curRune) {
+		if (curRune != '\\') && (prevRune == '\\') {
+			return "", ErrInvalidString
+		}
+		if (curRune != '\\') && (prevRune != 0) {
+			outString.WriteRune(prevRune)
+		}
+		prevRune = curRune
+		return outString.String(), nil
+	}
+
+	if prevRune == 0 {
+		return "", ErrInvalidString
+	}
+	if (prevRune == '\\') && !slash {
+		prevRune = curRune
+		return outString.String(), nil
+	}
+	if slash {
+		prevRune = '\\'
+	}
+	count, _ := strconv.Atoi(string(curRune))
+	if count != 0 {
+		outString.WriteString(strings.Repeat(string(prevRune), count))
+	}
+	prevRune = 0
+	return outString.String(), nil
+}
+
+func Unpack(inputString string) (string, error) {
+	var outString strings.Builder
+	slash = false
+	prevRune = 0
+	for _, curRune := range inputString {
+		s, err := unpackRune(curRune)
+		if err != nil {
+			return "", err
+		}
+		outString.WriteString(s)
+	}
+	if prevRune != 0 {
+		outString.WriteRune(prevRune)
+	}
+	return outString.String(), nil
 }
