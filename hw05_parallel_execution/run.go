c68075290e70501002c9d33020b2b1e68fbb9f00
Gumyarov Marat <brisk@inbox.ru>
2021-11-14 14:29:54 +0300

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,86 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+func worker(task Task, c chan error, q chan struct{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case c <- task():
+			return
+		case <-q:
+			return
+		}
+	}
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	var errCount int
+	var curTask int
+	var activeWorkers int
+	var tasksCompleted int
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+	tasksCount := len(tasks)
+
+	c := make(chan error, n)
+	q := make(chan struct{})
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			err := <-c
+			mu.Lock()
+			activeWorkers--
+			tasksCompleted++
+			if err != nil {
+				errCount++
+			}
+			if (errCount >= m) || (tasksCompleted >= tasksCount) {
+				mu.Unlock()
+				close(q)
+				break
+			}
+			mu.Unlock()
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			mu.Lock()
+			if activeWorkers >= n {
+				mu.Unlock()
+				continue
+			}
+			mu.Unlock()
+			task := tasks[curTask]
+			wg.Add(1)
+			go worker(task, c, q, &wg)
+			mu.Lock()
+			curTask++
+			activeWorkers++
+			if (curTask >= tasksCount) || (errCount >= m) {
+				mu.Unlock()
+				break
+			}
+			mu.Unlock()
+		}
+	}()
+	<-q
+	wg.Wait()
+
+	if errCount >= m {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -3,13 +3,13 @@ package hw05parallelexecution
 import (
 	"errors"
 	"fmt"
+	"go.uber.org/goleak"
 	"math/rand"
 	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"go.uber.org/goleak"
 )
 
 func TestRun(t *testing.T) {
@@ -66,5 +66,56 @@ func TestRun(t *testing.T) {
 
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+
+	t.Run("max errors 0", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := 0
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
+	})
+
+	t.Run("check concurrency without time.Sleep", func(t *testing.T) {
+		tasksCount := 5
+		workersCount := tasksCount
+		var startedTasks int32
+		tasks := make([]Task, 0, tasksCount)
+
+		c := make(chan struct{}, tasksCount)
+		for i := 0; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				atomic.AddInt32(&startedTasks, 1)
+				<-c
+				return nil
+			})
+		}
+
+		go func(workersCountLocal int, tasksCountLocal int) {
+			_ = Run(tasks, workersCount, tasksCount)
+		}(workersCount, tasksCount)
+
+		for i := 0; i < tasksCount; i++ {
+			c <- struct{}{}
+		}
+
+		require.Eventually(t, func() bool {
+			return atomic.LoadInt32(&startedTasks) == int32(workersCount)
+		}, 3*time.Second, time.Millisecond, "tasks were run sequentially?")
 	})
 }
