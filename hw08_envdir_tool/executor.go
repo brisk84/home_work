57058f05a0ebe83e716330322c63571e737e9a57
Gumyarov Marat <brisk@inbox.ru>
2021-11-29 01:11:05 +0300

    HW08 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func ExecuteCmd(name string, arg ...string) *exec.Cmd {
+	return exec.Command(name, arg...)
+}
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	cmde := ExecuteCmd(cmd[0], cmd[1:]...)
+	cmde.Stdin = os.Stdin
+	cmde.Stdout = os.Stdout
+	cmde.Stderr = os.Stderr
+	cmde.Env = os.Environ()
+
+	for k, v := range env {
+		if v.NeedRemove {
+			var newEnv []string
+			for _, oldElem := range cmde.Env {
+				if !strings.Contains(oldElem, k) {
+					newEnv = append(newEnv, oldElem)
+				}
+			}
+			cmde.Env = newEnv
+			continue
+		}
+		cmde.Env = append(cmde.Env, k+"="+v.Value)
+	}
+
+	if err := cmde.Run(); err != nil {
+		fmt.Println("Error: ", err)
+	}
+	return cmde.ProcessState.ExitCode()
 }
